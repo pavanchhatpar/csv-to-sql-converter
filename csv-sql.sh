9e8f90a2794f1283290933c3612bf8d78f46132f
William Desportes <williamdes@wdes.fr>
2020-04-22 16:18:45 +0200

    Support files with spaces in the name

--- csv-sql.sh
+++ csv-sql.sh
@@ -1,6 +1,6 @@
-fname=$1
-sed 's/\s*,*\s*$//g' $fname > tmp.csv
-op=$(echo $fname | cut -d"." -f 1)
+fname="$1"
+sed 's/\s*,*\s*$//g' "$fname" > tmp.csv
+op=$(echo "$fname" | cut -d"." -f 1)
 opfile="$op.sql"
 op="\`$op\`"
 columns=$(head --lines=1 tmp.csv | sed 's/,/`,`/g' | tr -d "\r\n")
@@ -9,5 +9,5 @@ tail --lines=+2 tmp.csv | while read l ; do
 values=$(echo $l | sed 's/,/\",\"/g' | tr -d "\r\n")
 values="\"$values\""
 echo "INSERT INTO $op($columns) VALUES ($values);"
-done > $opfile
+done > "$opfile"
 rm tmp.csv
