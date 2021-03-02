ed094d0404856f031f27630153e8f5b53a7f8ed7
AnalystXF <ben31908@gmail.com>
2021-03-02 11:30:30 +0800

    [fix] Solve the problem that macOS doesn't work
    --line param is not support in MacOS

--- csv-sql.sh
+++ csv-sql.sh
@@ -3,9 +3,9 @@ sed 's/\s*,*\s*$//g' "$fname" > tmp.csv
 op=$(echo "$fname" | cut -d"." -f 1)
 opfile="$op.sql"
 op="\`$op\`"
-columns=$(head --lines=1 tmp.csv | sed 's/,/`,`/g' | tr -d "\r\n")
+columns=$(head -n 1 tmp.csv | sed 's/,/`,`/g' | tr -d "\r\n")
 columns="\`$columns\`"
-tail --lines=+2 tmp.csv | while read l ; do
+tail -n +2 tmp.csv | while read l ; do
 values=$(echo $l | sed 's/,/\",\"/g' | tr -d "\r\n")
 values="\"$values\""
 echo "INSERT INTO $op($columns) VALUES ($values);"
